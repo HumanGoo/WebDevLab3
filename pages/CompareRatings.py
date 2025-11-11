a37b0762673986b1b4546daa291e6bea0e565280
beaucejourdanielle-coder <beaucejourdanielle@gmail.com>
2025-11-11 16:11:43 -0500

    Enhance Compare Ratings page with multi-select and chart
    Updated the Compare Ratings page to allow users to select multiple book titles and compare their ratings. Added functionality to fetch ratings from the Open Library API and display them in a bar chart.

--- pages/CompareRatings.py
+++ pages/CompareRatings.py
@@ -1,14 +1,45 @@
 import streamlit as st
 import requests as r
+import pandas as pd
 
-st.title("Library")
+#Compare Ratings Page 
 
-baseURL  = "https://openlibrary.org/search.json?q="
+st.title("Compare Book Ratings! 📚")
+st.write("Choose books from the drop-down menu and compare ratings between these five popular reads!")
+
+st.image("Images/Books.jpg")
+
+option = st.multiselect("Select Titles", ["Harry Potter and the Sorcerer's Stone", "1984", "To Kill a Mockingbird", "Moby Dick", "The Lord of the Rings"])
+
+baseURL = "https://openlibrary.org/search.json?q="
+
+ratingsStored = []
+
+# may need to make this dynamic 
+
+if option:
+    for book in option:
+        newURL = baseURL + book + "&fields=*,availability&limit=1"
+        response = r.get(newURL)
+        data = response.json()
+
+        if data.get("docs"):
+            docs = data.get("docs")[0]
+            title = docs.get("title", "")
+            rating = docs.get("ratings_average", None)
+
+            if rating == None:
+                rating = 0
+                ratingsStored.append({"Book":book, "Rating":0})
+            else:
+                ratingsStored.append({"Book": title, "Rating":rating})
 
 
-Query = st.text_input("Search for any Book by title, author, and more!", placeholder= "I'm looking for...")
+    df = pd.DataFrame(ratingsStored)
+    df = df.set_index("Book")
 
-if Query:
-    request = r.get(baseURL + Query)
-    data = request.json()
-    st.write(data)
+    st.subheader("Ratings Bar Graph")
+    st.bar_chart(df)
+            
+
+
