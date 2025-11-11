71c01856e2b7b915d5fd47477536089ce25b7f24
Eugene Li <eugene.gao.li@gmail.com>
2025-11-10 23:27:01 -0500

    Library is Done

--- pages/Library.py
+++ pages/Library.py
@@ -4,15 +4,37 @@ import requests as r
 st.title("Library")
 
 baseURL  = "https://openlibrary.org/search.json?q="
+imageURL = "https://covers.openlibrary.org/b/id/"
 
 
 Query = st.text_input("Search for any Book by title, author, and more!", placeholder= "I'm looking for...")
 
 if Query:
-    request = r.get(baseURL + Query)
+    count = 1
+    pageNumber = f"&page={count}"
+    request = r.get(baseURL + Query + pageNumber)
     data = request.json()
+    amount = f"{data['numFound']:,}"
+    col1, col2, filler = st.columns([3,4,1])
+    resultHits = len(data['docs'])
+    with col1:
+        st.write(f"{amount} hits")
+    with col2:
+        st.write(f"*showing {resultHits} out of {amount} results*")
+
     for result in data['docs']:
-        st.header(result["title"])
-        authors = ", ".join(result["author_name"])
-        st.write(f'by {authors}')
+        col1, col2 = st.columns([1,2])
+        with col1:
+            try:
+                st.image(f'{imageURL}{result["cover_i"]}-M.jpg')
+            except:
+                st.image("Images/Message_Not_Found.jpg")
+        with col2:  
+            st.header(result["title"])
+            try:
+                authors = ", ".join(result["author_name"])
+            except:
+                authors = "Unknown"
+            st.subheader(f'by {authors}')
+            st.write(f"First published in {result['first_publish_year']}")
         st.divider()
