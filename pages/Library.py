9517b66cbf67f2d80b4d041ba41917d0045edaa7
Eugene Li <eugene.gao.li@gmail.com>
2025-11-11 00:14:06 -0500

    Update Library.py

--- pages/Library.py
+++ pages/Library.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests as r
+from math import ceil
 
 st.title("Library")
 
@@ -7,21 +8,40 @@ baseURL  = "https://openlibrary.org/search.json?q="
 imageURL = "https://covers.openlibrary.org/b/id/"
 
 
-Query = st.text_input("Search for any Book by title, author, and more!", placeholder= "I'm looking for...")
-
+if 'query' not in st.session_state:
+    st.session_state.query = ""
+Query = st.text_input("Search for any Book by title, author, and more!", value= st.session_state.query, placeholder= "I'm looking for...")
+st.session_state.query = Query
+if 'count' not in st.session_state:
+    st.session_state.count = 1
+count = st.session_state.count
 if Query:
-    count = 1
-    pageNumber = f"&page={count}"
+    
+    pageNumber = f"&page={st.session_state.count}"
     request = r.get(baseURL + Query + pageNumber)
     data = request.json()
     amount = f"{data['numFound']:,}"
-    col1, col2, filler = st.columns([3,4,1])
-    resultHits = len(data['docs'])
+    col1, col2, filler = st.columns([3,4,2])
+    resultHits = len(data['docs']) + (100 * (count-1))
+    firstEntry = 1+(100 * (count-1))
+    maxPage = ceil(data['numFound']/100)
     with col1:
         st.write(f"{amount} hits")
     with col2:
-        st.write(f"*showing {resultHits} out of {amount} results*")
-
+        if len(data['docs']):
+            firstEntry = 1+(100 * (count-1))
+        else:
+            firstEntry = 0
+        st.write(f"*showing {firstEntry} - {resultHits} out of {amount} results*")
+    with filler:
+        try:
+            newcount = st.number_input(f"Page {st.session_state.count} out of {maxPage}", min_value=0, max_value=maxPage,value=st.session_state.count,
+                                        step=1)
+        except:
+            newcount = count
+        st.session_state.count = newcount
+        if st.session_state.count != count:
+            st.rerun()
     for result in data['docs']:
         col1, col2 = st.columns([1,2])
         with col1:
