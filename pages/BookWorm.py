f05910283bc919d095d1201e4dfb7418b9915e4e
Eugene Li <eugene.gao.li@gmail.com>
2025-11-13 19:35:39 -0500

    Update BookWorm.py

--- pages/BookWorm.py
+++ pages/BookWorm.py
@@ -7,5 +7,5 @@ st.title("BookWorm")
 
 baseURL  = "https://openlibrary.org/search.json?q="
 
-st.write("This is the page for which we are implementing our chatbot, BookWorm")
+st.write("This is the page for which we are implementing our chatbot, BookWorm.")
 #Query = st.text_input("Search for any Book by title, author, and more!", placeholder= "I'm looking for...")
