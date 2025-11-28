4d3d9f87a8b2ca5e312e998371ffe841ec12f6e0
Eugene Li <eugene.gao.li@gmail.com>
2025-11-28 00:32:14 -0500

    Update BookWorm.py

--- pages/BookWorm.py
+++ pages/BookWorm.py
@@ -41,7 +41,7 @@ if st.checkbox("Looking for a Specific Author"):
 
 content = st.text_input("What Kind of a Book Are You Looking For? i.e Fiction, Thriller, Happy, Educational, Mars etc.", 
                         placeholder= "Tags")
-start_color, end_color = st.select_slider(
+startTime, endTime = st.select_slider(
     "Select a range of your desired book's publish year",
     options=aList,
     value=(1900, 2025),
@@ -49,10 +49,18 @@ start_color, end_color = st.select_slider(
 
 model = genai.GenerativeModel('models/gemini-2.5-flash') #this is the free model of google gemini
 if st.button("Give Me a Book!"):
-    query = ""
-    response = r.get(baseURL+"&mode=everything")
+    query = content.strip().replace(" ","").lower()
+    publishYear = f"+first_publish_year:[%3A%5B{startTime}+TO+{endTime}]%5D"
+    query += publishYear
+    if author:
+        query+= f"+author3A{author}"
+    data = r.get(baseURL+query+"&mode=everything")
+    aDict = data.json()
+    st.write(aDict)
+    """
     response = model.generate_content(content)
     text = response.text
+    """
 
 try:
     def stream_data():
