241cd6e89e905b94e6917a4b8f11e1e072216fed
Eugene Li <eugene.gao.li@gmail.com>
2025-11-27 23:35:19 -0500

    Update BookWorm.py

--- pages/BookWorm.py
+++ pages/BookWorm.py
@@ -14,22 +14,32 @@ baseURL  = "https://openlibrary.org/search.json?q="
 
 st.write("Welcome to BookWorm! Find the right book for you.")
 
+"""
+Find a book based on the filters passed through, and then have the LLM
+explain why it is the right book for the person
+"""
 
-
+"""
+PLANNED FILTERS:
+length of book
+genre/subject?
+vibes of book
+"""
 
 
 key = st.secrets["key"]
 genai.configure(api_key=key)
 
-st.slider("Book Length",min_value=10, max_value=1000)
-st.checkbox("asfdklksd")
+length = st.slider("Desired Book Length",min_value=10, max_value=1000)
+subject = st.text_input("Subject/Genre")
+if st.checkbox("Looking for a Specific Author"):
+    author = st.text_input("Author")
 
 
 content = st.text_input("What Kind of Book Are You Looking For?")
 
+
 model = genai.GenerativeModel('models/gemini-2.5-flash') #this is the free model of google gemini
-
-
 if st.button("Give Me a Book!"):
     response = model.generate_content(content)
     text = response.text
