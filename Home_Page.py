6e787c2cc319698d26bc25a6f4bb129f739afc3e
Eugene Li <eugene.gao.li@gmail.com>
2025-11-30 16:26:15 -0500

    s

--- Home_Page.py
+++ Home_Page.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 with st.sidebar:
     st.sidebar.page_link("Home_Page.py", label="🏠Home Page🏠")
-    st.sidebar.page_link("pages/Library.py", label="📚Library📖")
-    st.sidebar.page_link("pages/CompareRatings.py", label="📊Book Comparisons📈")
-    st.sidebar.page_link("pages/BookWorm.py", label="📚BookWorm Book Recommendations🐛")
-    st.sidebar.page_link("pages/BookBot", label="🤖BookBot🤖")
+    st.sidebar.page_link("pages\Library.py", label="📚Library📖")
+    st.sidebar.page_link("pages\CompareRatings.py", label="📊Book Comparisons📈")
+    st.sidebar.page_link("pages\BookWorm.py", label="📚BookWorm Recommendations🐛")
+    st.sidebar.page_link("pages\BookBot", label="🤖BookBot🤖")
+
 
 # Title of App
 st.title("Web Development Lab03")
--- pages/BookBot.py
+++ pages/BookBot.py
@@ -5,10 +5,11 @@ import time
 
 with st.sidebar:
     st.sidebar.page_link("Home_Page.py", label="🏠Home Page🏠")
-    st.sidebar.page_link("pages/Library.py", label="📚Library📖")
-    st.sidebar.page_link("pages/CompareRatings.py", label="📊Book Comparisons📈")
-    st.sidebar.page_link("pages/BookWorm.py", label="📚BookWorm Book Recommendations🐛")
-    st.sidebar.page_link("pages/BookBot", label="🤖BookBot🤖")
+    st.sidebar.page_link("pages\Library.py", label="📚Library📖")
+    st.sidebar.page_link("pages\CompareRatings.py", label="📊Book Comparisons📈")
+    st.sidebar.page_link("pages\BookWorm.py", label="📚BookWorm Recommendations🐛")
+    st.sidebar.page_link("pages\BookBot", label="🤖BookBot🤖")
+
 
 
 
