7c2e12c1ff8708870334defa12d8b83c9d351268
Eugene Li <eugene.gao.li@gmail.com>
2025-11-30 16:31:10 -0500

    d
    d

--- pages/BookWorm.py
+++ pages/BookWorm.py
@@ -8,8 +8,8 @@ with st.sidebar:
     st.sidebar.page_link("Home_Page.py", label="🏠Home Page🏠")
     st.sidebar.page_link("pages/Library.py", label="📚Library📖")
     st.sidebar.page_link("pages/CompareRatings.py", label="📊Book Comparisons📈")
-    st.sidebar.page_link("pages/BookWorm.py", label="📚BookWorm Recommendations🐛")
-    st.sidebar.page_link("pages/BookBot.py", label="🤖BookBot🤖")
+    st.sidebar.page_link("pages/BookWorm.py", label="You are Here!")
+    st.sidebar.page_link("pages/BookBot.py", label="🤖BookBot™")
 
 currentDate = date.today()
 currentYear = currentDate.year
--- pages/Library.py
+++ pages/Library.py
@@ -4,10 +4,10 @@ from math import ceil
 
 with st.sidebar:
     st.sidebar.page_link("Home_Page.py", label="🏠Home Page🏠")
-    st.sidebar.page_link("pages/Library.py", label="📚Library📖")
+    st.sidebar.page_link("pages/Library.py", label="You are Here!")
     st.sidebar.page_link("pages/CompareRatings.py", label="📊Book Comparisons📈")
     st.sidebar.page_link("pages/BookWorm.py", label="📚BookWorm Recommendations🐛")
-    st.sidebar.page_link("pages/BookBot.py", label="🤖BookBot🤖")
+    st.sidebar.page_link("pages/BookBot.py", label="🤖BookBot™")
 
 st.title("Library")
 
